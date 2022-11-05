929ef2ebd69d45b96ba89cb32e8ea4ec25d48df7
ilias-zet <iliaszerman@gmail.com>
2022-11-05 15:43:19 +0300

    Client configured

--- client/src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-function App() {
-  return (
-    <div>lol</div>
-  );
-}
-
-export default App;
---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react';
+import Header from './components/Header';
+
+const App: React.FC = () => {
+  return (
+    <Header />
+  );
+}
+
+export default App;
