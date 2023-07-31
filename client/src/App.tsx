d7edc56adeb15ab24cc782655fef99fc54298454
Karim Zerman <whiskaswannacry@gmail.com>
2023-07-31 19:30:22 +0300

    Deleted

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,1 @@
-import './App.css';
-import React from 'react';
-import Header from './components/Header';
 
-const App: React.FC = () => {
-  return (
-    <Header />
-  );
-}
-
-export default App;
