5d24f6d83242239cb235926c61246d15fffe1e6a
Karim Zerman <whiskaswannacry@gmail.com>
2023-08-03 01:08:00 +0300

    Errors fixed
    Debugging logs removed, fixed post request, check for port added

--- backend/index.js
+++ backend/index.js
@@ -1,62 +1,67 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const { Product, User } = require('./models');
-const productsRoutes = require('./routes/products')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const { Product, User } = require("./models");
+const productsRoutes = require("./routes/products");
+const cors = require('cors');
+require("dotenv").config();
 
 const app = express();
+app.use(cors())
 
 const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
-if(!MONGO_URL) {
-  throw new Error(".env Error: Some variable is not defined or does not exist")
+if (!MONGO_URL) {
+  throw new Error(".env Error: Variable 'MONGO_URL' is not defined or does not exist");
 }
-console.log(Product)
-async function start() {
-    try{
-        await mongoose.connect(MONGO_URL, {
-            useNewUrlParser:true,
-            useUnifiedTopology: true,
-        })
-        console.log("Server has been conected to Mongo DB")
-        
-        app.listen(PORT, () => {
-            console.log('Server has been started on port:', PORT)
-        })
-    } catch(e) {
-        console.log("Server error: ", e)
-        process.exit(1)
-    }
+if (!PORT) {
+  throw new Error(".env Error: Variable 'PORT'  is not defined or does not exist");
 }
 
-start()
+async function start() {
+  try {
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Server has been conected to Mongo DB");
+
+    app.listen(PORT, () => {
+      console.log("Server has been started on port:", PORT);
+    });
+  } catch (e) {
+    console.log("Server error: ", e);
+    process.exit(1);
+  }
+}
+
+start();
 
 // Conditional save user to DB
-app.get('/createUser',async (request, response) => {
-  const { userData } = request.query
-  const { name,surname,email,pass } = userData;
+app.post("/createUser", async (request, response) => {
+  const { userData } = request.query;
+  const { name, surname, email, pass } = userData;
+  let success;
   let user = await User.findOne({ email })
-  let succes;
-  if(user) {
-    console.log(user)
+  if(user){
+    return response.status(500).send({message : 'User with this e-mail is already signed up!'})
   }
   else {
-    let newUser = new User({ name,surname,email,pass })
     try {
-      await newUser.save();
-      succes = true;
-    } catch(e) {
-      console.log("User save error: ", e)
+      success = true; 
+      user = new User({ name, surname, email, pass });
+      await user.save();
+    } catch (e) {
+      console.log("User save error: ", e);
+      return response.status(500).send({message : 'Error saving user!'})
     }
   }
-  response.json({ succes,user })
-})
+  response.json({ success, user });
+});
 
 // Get all categories in DB
-app.get('/getAllCategories',async (request, response) => {
-  const categories = await Product.distinct("main_category")
-  response.json(categories)
-  console.log(categories)
-})
+app.get("/getAllCategories", async (request, response) => {
+  const categories = await Product.distinct("main_category");
+  response.json(categories);
+});
 
-app.use(productsRoutes)
+app.use(productsRoutes);
