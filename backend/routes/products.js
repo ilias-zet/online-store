429aded6f057ebc610120898990de37d5808a9bc
Karim Zerman <whiskaswannacry@gmail.com>
2023-08-02 17:53:59 +0300

    Model import fixed

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,21 +1,19 @@
 const express = require('express');
-const Product = require('../models')
+const { Product } = require('../models')
 const router = express.Router();
-
-// const app = express();
 
 // Get all products by a category
 router.get('/getProductsByCategory',async (request, response) => {
   const { category } = request.query
   let products = await Product.find({"main_category":category}).limit(100)
-  await response.json(products)
+  response.json(products)
 })
 
 // Get all information about a product. The return value - product
 router.get('/getProductAllInformation',async (request, response) => {
   const { product_id } = request.query
   let product = await Product.find({"_id":product_id})
-  await response.json(product)
+  response.json(product)
 })
 
 module.exports = router;
