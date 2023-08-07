f1c2798add66bb2dc8168ebdd71e2f40b963875b
Karim Zerman <whiskaswannacry@gmail.com>
2023-08-07 18:39:49 +0300

    Wrong Require for id removed

--- backend/models.js
+++ backend/models.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-  _id: {
-    type: Number,
-    required: true,
-  },
+
   title: {
     type: String,
     required: true,
